eac8c6aeb942998349632033c5984713f14ba8d1
f11m4at4 <f11m4at4@gmail.com>
2022-05-12 16:50:52 +0900

    폭발효과 추가

--- Assets/Shooting/Enemy.cs
+++ Assets/Shooting/Enemy.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 // 아래로 계속 이동하고 싶다.
 // 타겟쪽으로 계속 이동하고 싶다.
 // 필요속성 : 타겟
+// 부딪혔을 때 폭발효과를 발생시키고 싶다.
+// 필요속성 : 폭발효과 공장
 public class Enemy : MonoBehaviour
 {
     // 필요속성 : 속도
     public float speed = 5;
     // 필요속성 : 타겟
     public GameObject target;
+    // 필요속성 : 폭발효과 공장
+    public GameObject explosionFactory;
 
     // Start is called before the first frame update
     void Start()
@@ -31,8 +35,13 @@ public class Enemy : MonoBehaviour
     }
 
     // 다른 물체와 부딪히면 
+    // 부딪혔을 때 
     private void OnCollisionEnter(Collision collision)
     {
+        // 폭발효과를 발생시키고 싶다.
+        GameObject explosion = Instantiate(explosionFactory);
+        explosion.transform.position = transform.position;
+
         // 갸도 죽고 
         Destroy(collision.gameObject);
         // 나도 죽고싶다.
